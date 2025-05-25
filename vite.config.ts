7f5efb8e66112d4c49972c44e557428bf0e36c2a
Daniel Ulied <danielulied@Daniels-MacBook-Air.local>
2025-05-25 21:49:41 +0200

    last...

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  base: '/birthdate-buddies-calendar/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/birthdate-buddies-calendar/' : '/',
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  
-});
-
+}));
